bed401cf3f2fa6226b06d4f7d3d1c37ca5689539
Gautier Lefebvre <gautier.lfbvr@gmail.com>
2017-06-12 15:15:50 +0200

    listen for list of stores

--- src/Connect.jsx
+++ src/Connect.jsx
@@ -57,17 +57,6 @@ module.exports = (...params) => Component => createReactClass({
     return this.getStateFromFlux();
   },
 
-  getProps(props) {
-    // if the parameter is a string, return an array with one entry
-    // else if the parameter is an array, return the parameter
-    // else return null
-    return typeof props === 'string' ?
-      [ props ]
-      : Array.isArray(props) ?
-        props
-        : null;
-  },
-
   componentWillMount() {
     const flux = this.getFlux();
 
@@ -81,23 +70,37 @@ module.exports = (...params) => Component => createReactClass({
     // for each given store
     // listen to the given event (or 'change' by default) and update the state
     forEach(params, (storeAndState) => {
-      const store = flux.store(storeAndState.store);
-      const events = storeAndState.event || 'change';
+      // get the store or list of stores
+      const store = Array.isArray(storeAndState.store) ?
+        storeAndState.store.map(storeName => flux.store(storeName))
+        : flux.store(storeAndState.store);
+
       const watchedProps = this.getProps(storeAndState.watchedProps);
 
       const reference = (props) => {
-        props = props || this.props;
-
         // we check if the component is still mounted just in case
         if (this.mounted) {
-          this.setState(storeAndState.state.call(this, store, props));
+          const p = props || this.props;
+
+          // store is either an array or a single store
+          this.setState(storeAndState.state.call(this, store, p));
         }
       };
 
-      forEach(Array.isArray(events) ? events : [ events ], event => {
-        // listen to the event
-        store.on(event, reference);
-      })
+      const events = this.getEvents(storeAndState.event, store);
+
+      // listen for the events
+      if (Array.isArray(store)) {
+        forEach(store, (s, idx) => {
+          forEach(events[idx], (event) => {
+            s.on(event, reference);
+          });
+        });
+      } else {
+        forEach(events, (event) => {
+          store.on(event, reference);
+        });
+      }
 
       // we store the callback reference so we can unsubscribe later
       this.callbacks.push({ store, reference, events, watchedProps });
@@ -106,30 +109,22 @@ module.exports = (...params) => Component => createReactClass({
 
   componentWillReceiveProps(nextProps) {
     // for each watched store, update if a watched prop has changed
-    forEach(this.callbacks, callback => {
+    forEach(this.callbacks, (callback) => {
       let hasChanged = false;
-
-      // if the wachedProps is set to true, we compare the entire props object
-      // else we compare only the specified props, if any
-      const propsToCompare = (
-        isBoolean(callback.watchedProps) && callback.watchedProps ?
-          keys(this.props)
-          : Array.isArray(callback.watchedProps) && !!callback.watchedProps.length ?
-            callback.watchedProps
-            : null
-      );
 
       // check each watched prop individually
       // we can't use 'pick(nextProps, callback.watchedProps)
       // because it does not work with nested prop names ('props.foo.bar')
-      forEach(propsToCompare, propName => {
+      forEach(callback.watchedProps, (propName) => {
         const nextWatchedProp = get(nextProps, propName);
         const currentWatchedProp = get(this.props, propName);
 
         // if a prop changed, call the given 'state' function with next props
         if (!isEqual(nextWatchedProp, currentWatchedProp)) {
-          return !(hasChanged = true);
+          hasChanged = true;
         }
+
+        return !hasChanged;
       });
 
       if (hasChanged) {
@@ -143,9 +138,17 @@ module.exports = (...params) => Component => createReactClass({
 
     // we unsubscribe from the events we subscribed to in componentWillMount
     forEach(this.callbacks, (callback) => {
-      forEach(callback.events, (event) => {
-        callback.store.removeListener(callback.event, callback.reference);
-      });
+      if (Array.isArray(callback.store)) {
+        forEach(callback.store, (s, idx) => {
+          forEach(callback.events[idx], (event) => {
+            s.removeListener(event, callback.reference);
+          });
+        });
+      } else {
+        forEach(callback.events, (event) => {
+          callback.store.removeListener(event, callback.reference);
+        });
+      }
     });
   },
 
@@ -160,11 +163,16 @@ module.exports = (...params) => Component => createReactClass({
         storeAndState.state.call(
           this,
           flux.store(storeAndState.store),
-          this.props
+          this.props,
         ),
       ),
       {},
     );
+  },
+
+  // returns the wrapped component so the parent component can use refs
+  getWrappedInstance() {
+    return this.wrappedInstance;
   },
 
   // sets the wrapped component using react refs
@@ -172,9 +180,38 @@ module.exports = (...params) => Component => createReactClass({
     this.wrappedInstance = el;
   },
 
-  // returns the wrapped component so the parent component can use refs
-  getWrappedInstance() {
-    return this.wrappedInstance;
+  getProps(props) {
+    // if the parameter is a string, return an array with one entry
+    // else if the parameter is an array, return the parameter
+    // else return null
+    if (Array.isArray(props) && !!props.length) {
+      return props;
+    } else if (typeof props === 'string') {
+      return [props];
+    } else if (isBoolean(props) && !!props) {
+      return keys(this.props);
+    }
+
+    return null;
+  },
+
+  getEvents(events, store) {
+    if (!Array.isArray(store)) {
+      if (Array.isArray(events)) {
+        return events;
+      } else if (typeof events === 'string') {
+        return [events];
+      }
+
+      return ['change'];
+    }
+
+    // if no events, listen for 'change' on every store
+    if (!events) {
+      return store.map(() => (['change']));
+    }
+
+    return store.map((s, idx) => this.getEvents(events[idx], s));
   },
 
   render() {
@@ -188,4 +225,3 @@ module.exports = (...params) => Component => createReactClass({
     );
   },
 });
-
